ffc9b4cc8583d32e4b769ec67a6c6ded74e02fa7
Matteo Mattei <matteo.mattei@gmail.com>
2014-08-12 21:12:25 +0200

    fix deploy destination path

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,1 +1,1 @@
-jekyll build && rsync -v -rz --checksum --delete _site/ web1:/var/www/nginx/matteomattei.com/
+jekyll build && rsync -v -rz --checksum --delete _site/ web1:/var/www/nginx/matteomattei.com/public_html/
