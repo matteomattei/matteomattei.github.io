f9841861f3a3c35464bcc69ee6197973a79ac87f
Matteo Mattei <matteo.mattei@gmail.com>
2016-07-19 17:33:49 +0200

    minify css

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,2 +1,3 @@
+yui-compressor --type css public/css/hyde.css -o public/css/hyde.min.css
 jekyll build && rsync -v -rz --checksum --delete _site/ web1:/var/www/matteomattei.com/public_html/
 ssh web1 chown web5000:web5000 -R /var/www/matteomattei.com/public_html/
